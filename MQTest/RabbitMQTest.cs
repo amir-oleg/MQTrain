aa2b12d8dcb2f3f6086857083fda9801dd3837b3
amir-oleg <amir-oleg@yandex.ru>
2021-06-24 18:59:13 +0400

    Add project files.

---
+++ MQTest/RabbitMQTest.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MQTest
+{
+    [TestClass]
+    public class RabbitMQTest
+    {
+        private const string FileQueue = "Files";
+        private const string StatusQueue = "Status";
+        private const int MaxMessageSize = 1048576;
+        private readonly byte[] StatusWaiting = Encoding.UTF8.GetBytes(Environment.UserName + " Waiting...");
+        private readonly byte[] StatusUploading = Encoding.UTF8.GetBytes(Environment.UserName + " Uploading...");
+        [TestMethod]
+        public void SendFileTest()
+        {
+            const string filePath = @"F:/bandicam 2020-06-08 02-49-31-021.mp4";
+            var factory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "guest",
+                    Password = "guest",
+                    Port = 5672,
+                    RequestedConnectionTimeout = TimeSpan.FromMilliseconds(3000)
+                };
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(FileQueue, false, false, false, null);
+                        channel.QueueDeclare(StatusQueue, false, false, false, null);
+                        channel.BasicPublish("", StatusQueue, null, StatusUploading);
+                        var messageId = Guid.NewGuid() + Path.GetFileName(filePath);
+                        using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                        {
+                            var count = 0;
+                            for (var offset = 0; offset < stream.Length; offset += MaxMessageSize)
+                            {
+                                var body = new byte[MaxMessageSize];
+                                var props = channel.CreateBasicProperties();
+                                props.Headers = new Dictionary<string, object>
+                                {
+                                    {"Position", count}
+                                };
+
+                                if (offset + MaxMessageSize >= stream.Length)
+                                {
+                                    var lastBody = new byte[stream.Length - offset];
+                                    props.Headers.Add("End", true);
+                                    stream.Position = offset;
+                                    stream.Read(lastBody, 0, (int) (stream.Length - offset));
+                                    props.MessageId = messageId;
+                                    channel.BasicPublish("", FileQueue, props, lastBody);
+                                    Console.WriteLine($@"Number: {count}");
+                                    channel.BasicPublish("", StatusQueue, null, StatusWaiting);
+                                    return;
+                                }
+
+                                stream.Position = offset;
+                                stream.Read(body, 0, MaxMessageSize);
+                                Console.WriteLine($@"Number: {count}");
+                                props.MessageId = messageId;
+                                channel.BasicPublish("", FileQueue, props, body);
+                                count++;
+                            }
+                        }
+                        channel.BasicPublish("", StatusQueue, null, StatusWaiting);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ServerSide
+{
+    internal static class Program
+    {
+        private const string FileQueue = "Files";
+        private const string StatusQueue = "Status";
+        private static readonly Dictionary<string, List<byte[]>> Files = new Dictionary<string, List<byte[]>>();
+
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672,
+                RequestedConnectionTimeout = TimeSpan.FromMilliseconds(3000)
+            };
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(FileQueue, false, false, false, null);
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += ConsumerOnReceived;
+                        while (true)
+                        {
+                            channel.BasicConsume(StatusQueue, true, consumer);
+                            channel.BasicConsume(FileQueue, true, consumer);
+                            Thread.Sleep(500);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void ConsumerOnReceived(object sender, BasicDeliverEventArgs e)
+        {
+            if (!(sender is EventingBasicConsumer consumer))
+            {
+                return;
+            }
+
+            switch (e.RoutingKey)
+            {
+                case FileQueue:
+                {
+                    FileOnReceived(consumer, e);
+                    break;
+                }
+                case StatusQueue:
+                {
+                    StatusOnReceived(e);
+                    break;
+                }
+                default:
+                    throw new ArgumentException($"Queue {e.RoutingKey} doesn't support");
+            }
+        }
+
+        private static void FileOnReceived(EventingBasicConsumer consumer, BasicDeliverEventArgs e)
+        {
+            if (!(e.BasicProperties.Headers.TryGetValue("Position", out var pos)) || !int.TryParse(pos.ToString(), out var position))
+            {
+                return;
+            }
+            if (!Files.ContainsKey(e.BasicProperties.MessageId))
+            {
+                Files.Add(e.BasicProperties.MessageId, new List<byte[]>());
+            }
+            Files[e.BasicProperties.MessageId].Insert(position, e.Body.ToArray());
+            if (e.BasicProperties.Headers.TryGetValue("End", out var end) && bool.TryParse(end.ToString(), out var res) && res)
+            {
+                var fileBody = new List<byte>();
+                foreach (var filePart in Files[e.BasicProperties.MessageId])
+                {
+                    fileBody.AddRange(filePart);
+                }
+                File.WriteAllBytes($@"H:/{e.BasicProperties.MessageId}", fileBody.ToArray());
+                Files[e.BasicProperties.MessageId].Clear();
+                Files.Remove(e.BasicProperties.MessageId);
+            }
+            else
+            {
+                //consumer.Model.BasicConsume(FileQueue, true, consumer);
+            }
+        }
+
+        private static void StatusOnReceived(BasicDeliverEventArgs e)
+        {
+            Console.WriteLine($"{Encoding.UTF8.GetString(e.Body.ToArray())}");
+        }
+    }
+}
